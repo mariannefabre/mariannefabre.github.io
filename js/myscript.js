cdb91f80d95a514b1d91de4ba48e6a843ecc1620
MarianneF <marianne.fabre@hotmail.fr>
2021-01-16 16:59:49 +0100

    Skills logo added

--- js/myscript.js
+++ js/myscript.js
@@ -5,6 +5,21 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+function toggleFullscreen() {
+  let frame = document.getElementById("fullscreen");
+  if (!document.fullscreenElement) {
+    frame.requestFullscreen().catch((err) => {
+      console.log(err);
+    });
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+let toggle = document.getElementById("toggleFullscreen");
+toggle.addEventListener("click", toggleFullscreen, true);
+
 /*
 
 HTML : 
