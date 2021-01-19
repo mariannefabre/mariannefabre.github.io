90a73fd7809eef5ab1203e88b529ddb7b071517c
MarianneF <marianne.fabre@hotmail.fr>
2021-01-19 16:40:57 +0100

    fix menu icon invisible on mobile

--- js/myscript.js
+++ js/myscript.js
@@ -1,41 +1,25 @@
 let sideNav = document.getElementById("mySidenav");
 
-function isNavOpen() {
-  return sideNav.style.width === "100%" ? true : false;
-}
-function openNav() {
-  sideNav.style.width = "100%";
-}
-function closeNav() {
-  sideNav.style.width = "0";
-}
+const openSideNav = function () {
+  function isNavOpen() {
+    return sideNav.style.width === "100%" ? true : false;
+  }
+  function openNav() {
+    sideNav.style.width = "100%";
+  }
+  function closeNav() {
+    sideNav.style.width = "0";
+  }
 
-/*  const openNavigationBar = function () {
   document.getElementById("nav-icon").classList.toggle("open");
   isNavOpen() ? closeNav() : openNav();
 };
 
+sideNav.addEventListener("click", openSideNav, true);
+
 document
   .getElementById("nav-icon")
-  .addEventListener("click", openNavigationBar, true); */
-
-document.getElementById("nav-icon").addEventListener(
-  "click",
-  () => {
-    document.getElementById("nav-icon").classList.toggle("open");
-    isNavOpen() ? closeNav() : openNav();
-  },
-  true
-);
-
-sideNav.addEventListener(
-  "click",
-  () => {
-    document.getElementById("nav-icon").classList.toggle("open");
-    isNavOpen() ? closeNav() : openNav();
-  },
-  true
-);
+  .addEventListener("click", openSideNav, true);
 
 function toggleFullscreen() {
   let frame = document.getElementById("memory-game");
